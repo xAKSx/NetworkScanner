65168968482ca071de0fff36bf5a225386682558
AKSAG <72139162+xAKSx@users.noreply.github.com>
2024-08-18 13:06:12 +0500

    Add files via upload

---
+++ requirements.py
@@ -0,0 +1,10 @@
+# requirements.py
+
+# Import necessary libraries
+import tkinter as tk
+from tkinter import ttk
+import customtkinter
+import nmap
+import socket
+import ipaddress
+from ttkthemes import ThemedStyle
